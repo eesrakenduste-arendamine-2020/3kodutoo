debed5d5cd7a0004a60c0ea4a100b13862070d9d
Urmas Kirsipuu <55189712+pambukk@users.noreply.github.com>
2020-05-03 13:42:01 +0300

    Tuhande eraldaja jms väikesed fixid

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.currentOperandTextElementAjalugu = currentOperandTextElementAjalugu
+        this.previousOperandTextElementAjalugu = previousOperandTextElementAjalugu
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
         this.clear()
@@ -34,7 +36,7 @@ class Calculator {
         let computation
         const prev = parseFloat(this.previousOperand)
         const current = parseFloat(this.currentOperand)
-        /*if (isNaN(prev) || isNaN(current)) return*/
+        if (isNaN(prev) || isNaN(current)) return
         switch (this.operation) {
             case '+':
                 computation = prev + current
@@ -86,40 +88,39 @@ class Calculator {
         const decimalDigits = stringNumber.split('.')[1]
         let integerDisplay
         if (isNaN(integerDigits)) {
-            integerDisplay = ''
+          integerDisplay = ''
         } else {
-            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+          integerDisplay = integerDigits.toLocaleString('et', { maximumFractionDigits: 0 })
         }
         if (decimalDigits != null) {
-            return `${integerDisplay}.${decimalDigits}`
+          return `${integerDisplay}.${decimalDigits}`
         } else {
-            return integerDisplay
+          return integerDisplay
         }
-    }
-  
-    updateDisplay() {
+      }
+    
+      updateDisplay() {
         this.currentOperandTextElement.innerText =
-            this.getDisplayNumber(this.currentOperand)
+          this.getDisplayNumber(this.currentOperand)
         if (this.operation != null) {
-            this.previousOperandTextElement.innerText =
-                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+          this.previousOperandTextElement.innerText =
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
         } else {
-            this.previousOperandTextElement.innerText = ''
+          this.previousOperandTextElement.innerText = ''
         }
-    }
-    /*
+      }
+
+    
     updateDisplay_ajalugu() {
-            this.currentOperandTextElementAjalugu.innerText =
-              this.getDisplayNumber(this.currentOperand)
-            if (this.operation != null) {
-              this.previousOperandTextElementAjalugu.innerText =
-                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
-            } else {
-              this.previousOperandTextElementAjalugu.innerText = ''
-            }
+    this.previousOperandTextElementAjalugu.innerText +=
+        this.getDisplayNumber(this.currentOperand) + "\n";
+           if (this.operation != null) {
+              this.previousOperandTextElementAjalugu.innerText +=
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}` +"\n";
+           }
           }
       
-      */
+      
 }
     const numberButtons = document.querySelectorAll('[data-number]')
     const operationButtons = document.querySelectorAll('[data-operation]')
@@ -129,21 +130,24 @@ class Calculator {
     const allClearButton = document.querySelector('[data-all-clear]')
     const previousOperandTextElement = document.querySelector('[data-previous-operand]')
     const currentOperandTextElement = document.querySelector('[data-current-operand]')
-//const currentOperandTextElementAjalugu = document.querySelector('[ajalugu-data-current-operand]')
+const currentOperandTextElementAjalugu = document.querySelector('[ajalugu-data-current-operand]')
+const previousOperandTextElementAjalugu = document.querySelector('[ajalugu-data-previous-operand]')
   
-     const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+     const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement, currentOperandTextElementAjalugu, previousOperandTextElementAjalugu)
   
   numberButtons.forEach(button => {
     button.addEventListener('click', () => {
       calculator.appendNumber(button.innerText)
-      calculator.updateDisplay()
+        calculator.updateDisplay()
+        //calculator.updateDisplay_ajalugu()
     })
   })
   
   operationButtons.forEach(button => {
     button.addEventListener('click', () => {
       calculator.chooseOperation(button.innerText)
-      calculator.updateDisplay()
+        calculator.updateDisplay()
+        calculator.updateDisplay_ajalugu()
     })
   })
 
@@ -152,18 +156,19 @@ class Calculator {
         calculator.chooseOperation(button.innerText)
         calculator.compute_symbols()
         calculator.updateDisplay()
-   //     calculator.updateDisplay_ajalugu()
+   calculator.updateDisplay_ajalugu()
     })
   })
   
   equalsButton.addEventListener('click', button => {
     calculator.compute()
-    calculator.updateDisplay()
+      calculator.updateDisplay()
+      calculator.updateDisplay_ajalugu()
   })
   
   allClearButton.addEventListener('click', button => {
     calculator.clear()
-    calculator.updateDisplay()
+      calculator.updateDisplay()
   })
   
   deleteButton.addEventListener('click', button => {
